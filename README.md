224f1a62414c3294df08ca229b37cef834a3d4ce
Miega <tthomas.wi@gmail.com>
2017-03-05 15:35:28 -0600

    Organization and code changes
    Added new CSS to fix padding in navbar
    Re-organized folders and files
    Added new gulpfile to create localhost web server. Please look at the
    README for more details.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # regal-foundation
 Website for clothing storefront.
+
+# Using Gulp
+
+Default Task
+------------
+
+    gulp
+
+Running the default task automatically watches your project folders for any changes and runs the accompanying task. For example, if you've elected to run tasks on your JavaScript, anytime you change a JavaScript file gulp will automatically run those tasks, including a browser refresh if you've included BrowserSync.
+
+CSS
+---
+    gulp styles
+
+Running the gulp styles task will run your selected CSS tasks once.
+
+JavaScript
+----------
+
+    gulp scripts
+
+Running the gulp scripts task will run your selected JavaScript tasks once.
+
+Images
+------
+
+    gulp images
+
+Running the gulp images task will run your selected image tasks once.
+
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('images', function(){
+  gulp.src('img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/img/'));
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('js/**/*.js')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('default', ['browser-sync'], function(){
+  gulp.watch("js/**/*.js", ['scripts']);
+  gulp.watch("*.html", ['bs-reload']);
+});
